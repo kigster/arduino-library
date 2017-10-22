636ae82f71c972bdc4567d25c046e10cff8b0663
Konstantin Gredeskoul <kigster@gmail.com>
2017-10-21 20:18:32 -0700

    Bump version

--- lib/arduino/library/version.rb
+++ lib/arduino/library/version.rb
@@ -1,8 +1,8 @@
 module Arduino
   module Library
-    VERSION = '0.1.0'
+    VERSION = '0.2.0'
     DESCRIPTION = <<-eof
-      A simple model encapsulation of an Arduino Library with to/from JSON transformations, and proper validations.)
+      A simple model encapsulation of an Arduino Library with to/from JSON and .properties transformations, and proper validations, library search, and much more.)
     eof
   end
 end
